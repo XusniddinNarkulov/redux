461d330d44bd8cbc3a9cda4e56d05aa68c1adf5c
XusniddinNarkulov <narkulof@gmail.com>
2022-05-04 13:50:31 +0500

    redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function arrayRemove(arr, value) {
+   return arr.filter(function (ele) {
+      return ele != value;
+   });
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// actions
+const addMusic = (name, time) => {
+   return {
+      type: "add",
+      music: {
+         name,
+         time,
+      },
+   };
+};
+
+const findMusicTime = (name, info) => {
+   return {
+      type: "find",
+      music: {
+         name,
+         info,
+      },
+   };
+};
+
+const removeMusic = (name, info) => {
+   return {
+      type: "remove",
+      music: {
+         name,
+         info,
+      },
+   };
+};
+
+// reducers
+const addMusicReducer = (list = [], obj) => {
+   if (obj.type === "add") {
+      return (list = [...list, obj.music]);
+   }
+   return list;
+};
+const findMusicReducer = (list = [], obj) => {
+   if (obj.type === "find") {
+      const mus = obj.music.info.addMusicReducer.find(
+         (val) => val.music.name == obj.music.name
+      );
+      return mus.music.time;
+   }
+   return list;
+};
+
+const removeMusicReducer = (list = [], obj) => {
+   if (obj.type === "remove") {
+      const mus = obj.music.info.addMusicReducer.find(
+         (val) => val.music.name == obj.music.name
+      );
+      return arrayRemove(list, mus);
+   }
+};
+
+let store = createStore(addMusicReducer);
+
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch(addMusic("hello", "2:44 "));
+store.dispatch(addMusic("hell01", "3:44 "));
+store.dispatch(findMusicTime("hello", store.getState()));
+store.dispatch(removeMusic("hello1", store.getState()));
+
+// const { createStore, combineReducers } = redux;
+
+// const departaments = combineReducers({ addHistory, removeHistory });
+
+// const store = createStore(departaments);
+
+// store.dispatch(addMusic("hello"));
+
+// console.log(store.getState());
